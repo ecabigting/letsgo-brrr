36b38c95fc282686c86d8b06ebae5f09d8d160f4
ecabigting <ericcabigting@outlook.com>
2024-10-05 17:03:10 +0400

    **complete the go server, html and websocket implementation**
    - main.go
      - completed the server setup with websocket and broadcasting functions
    - htmx/index.html
      - implement a simple html file that connects to the websocket using
    htmx
    - hardware.go
      - hardware implementation of gopsutil

--- devicemonitor/cmd/main.go
+++ devicemonitor/cmd/main.go
@@ -2,10 +2,46 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"time"
 
-	_ "github.com/ecabigting/letsgo-brrr/devicemonitor/internal/server"
+	"github.com/ecabigting/letsgo-brrr/devicemonitor/internal/server"
 )
 
 func main() {
 	fmt.Println(".. Loading devicemonitor")
+
+	svr := server.NewServer(10, "6969")
+	go func(s *server.Server) {
+		for {
+			timeStamp := time.Now().Format("2006-01-02 15:04:05")
+			sectionSys, sectionDisc, sectionCPU := s.GetHardwareData()
+
+			html := `
+      <div hx-swap-oob="innerHTML:#timestamps">
+          <table class='table table-striped table-hover table-sm'>
+            <tbody>
+              <tr>
+                <td> Current System time:</td>
+                <td>` + timeStamp + `</td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
+      <div hx-swap-oob="innerHTML:#system">` + sectionSys + `</div>
+      <div hx-swap-oob="innerHTML:#disk">` + sectionDisc + `</div>
+      <div hx-swap-oob="innerHTML:#cpu">` + sectionCPU + `</div>
+      `
+
+			s.Broadcast([]byte(html))
+
+			time.Sleep(3 * time.Second)
+		}
+	}(svr)
+	err := http.ListenAndServe(":"+svr.Port, &svr.Mux)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- devicemonitor/internal/server/server.go
+++ devicemonitor/internal/server/server.go
@@ -1,13 +1,147 @@
 package server
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/coder/websocket"
+	"github.com/ecabigting/letsgo-brrr/devicemonitor/internal/hardware"
+)
 
 type Server struct {
+	Port string
+	// to keep track of Subscribers
+	// we create a map of subscribers
+	// as anonymous struct
+	Subscribers             map[*Subscriber]struct{} // anonymous struct
 	Mux                     http.ServeMux
+	SubscriberMutex         sync.Mutex
 	SubscriberMessageBuffer int
 }
 
-func (s *Server) NewServer() {
-	s.SubscriberMessageBuffer = 10
-	s.Mux.Handle("/", http.FileServer(http.Dir("../../htmx")))
+type Subscriber struct {
+	// channel where we
+	// can send messages
+	// for the Subscriber
+	msgs chan []byte
+}
+
+func NewServer(sub int, port string) *Server {
+	s := &Server{
+		Port:                    port,
+		SubscriberMessageBuffer: sub,
+		Subscribers:             make(map[*Subscriber]struct{}),
+	}
+
+	s.Mux.Handle("/", http.FileServer(http.Dir("./htmx")))
+	s.Mux.HandleFunc("/ws", s.SubscribeHandler)
+	return s
+}
+
+func (s *Server) SubscribeHandler(w http.ResponseWriter, r *http.Request) {
+	err := s.Subscribe(r.Context(), w, r)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (s *Server) AddSubscriber(subscriber *Subscriber) {
+	// lets lock the subscriber via mutex
+	// before we do some changes
+	s.SubscriberMutex.Lock()
+	s.Subscribers[subscriber] = struct{}{}
+	// unlock the subscriber so
+	// other resources can update it
+	s.SubscriberMutex.Unlock()
+	fmt.Println("Added subscriber", subscriber)
+}
+
+// Subscribe by creating connections
+// that we can keep track and able to send
+// messages to that connection
+func (s *Server) Subscribe(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	// creating the connection
+	// via websockets using
+	// the Subscriber struct
+	var c *websocket.Conn
+	subscriber := &Subscriber{
+		msgs: make(chan []byte, s.SubscriberMessageBuffer),
+	}
+
+	// lets add
+	// the new connection
+	// to our server
+	s.AddSubscriber(subscriber)
+
+	c, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		return err
+	}
+
+	// close the connection
+	// after the execution
+	defer c.CloseNow()
+
+	// close using
+	// the implemention of the
+	// io closer for this
+	// context
+	ctx = c.CloseRead(ctx)
+
+	// lets loop thru
+	// the websockets connected
+	// whenever a new message is
+	// available
+	for {
+		select {
+		// lets write the new message
+		// on this new channel(msgs)
+		// in this new subscriber
+		// we just created
+		case msg := <-subscriber.msgs:
+			ctx, cancel := context.WithTimeout(ctx, time.Second)
+			defer cancel()
+			err := c.Write(ctx, websocket.MessageText, msg)
+			if err != nil {
+				return err
+			}
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+// broadcast all the messages
+// to all connected subscribers
+// via channels
+// note the usage of Mutex
+// to protect the thread
+func (s *Server) Broadcast(msg []byte) {
+	s.SubscriberMutex.Lock()
+	for subs := range s.Subscribers {
+		subs.msgs <- msg
+	}
+	s.SubscriberMutex.Unlock()
+}
+
+func (s *Server) GetHardwareData() (string, string, string) {
+	sectionSys, err := hardware.GetSystemSection()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	sectionDisc, err := hardware.GetDiskSection()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	sectionCPU, err := hardware.GetCpuSection()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return sectionSys, sectionCPU, sectionDisc
 }
